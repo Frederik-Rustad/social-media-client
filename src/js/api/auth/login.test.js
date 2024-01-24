30f528a5ad73b00b6094711d33c5c254375d38e1
Frederik-Rustad <frederik.rustad.89@gmail.com>
2024-01-24 16:19:58 +0100

    Create login.test.js

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,13 @@
+import { login } from "./login";
+
+const Token = "newtoken1234567890";
+const ValidEmail = "newuser@noroff.no";
+const ValidPassword = "newpassword";
+
+const UserLogin = {
+  email: ValidEmail,
+  password: ValidPassword,
+};
+
+const invalidEmail = "user@email.com";
+const invalidPassword = "12345";
