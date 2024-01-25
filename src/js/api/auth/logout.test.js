a531b50b718c07f71bf6c62f5ccd0e764616582a
Frederik-Rustad <frederik.rustad.89@gmail.com>
2024-01-25 15:43:24 +0100

    Update logout.test.js

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -6,7 +6,7 @@ global.localStorage = localStorageMock;
 
 describe("remove", () => {
   const token_Key = "token";
-  const token_Value = ["a2BhR7LpXq9sT3wF8zY0vN1c"];
+  const token_Value = "a2BhR7LpXq9sT3wF8zY0vN1c";
 
   beforeEach(() => {
     localStorageMock.removeItem.mockClear();
@@ -15,8 +15,8 @@ describe("remove", () => {
   it("removes the set token from localStorage", () => {
     save(token_Key, token_Value);
     expect(load(token_Key)).toEqual(token_Value);
-    expect(localStorageMock.getItem(token_Key)).toEqual(JSON.stringify(token_Value));
+    expect(localStorage.getItem(token_Key)).toEqual(JSON.stringify(token_Value));
     logout();
-    expect(localStorage.getItem(token_Key)).toBeNull();
+    expect(localStorage.getItem(token_Key)).toEqual(null);
    });
 });
