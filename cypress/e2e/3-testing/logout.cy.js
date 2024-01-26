0cd2bcae856ad9899fd1a6f1b47b722ada5eda8a
Frederik-Rustad <frederik.rustad.89@gmail.com>
2024-01-26 19:47:58 +0100

    Update logout.cy.js
    update based on peer feedback

--- cypress/e2e/3-testing/logout.cy.js
+++ cypress/e2e/3-testing/logout.cy.js
@@ -1,30 +1,30 @@
-describe("Logout function", () => {
+describe('Logout function', () => {
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit('/');
     cy.clearLocalStorage();
   });
 
-  it("should login and logout", () => {
-    cy.visit("/");
+  it('should login and logout', () => {
+    cy.visit('/');
     cy.wait(1000);
-    cy.get(".btn-close:visible").click();
+    cy.get('.btn-close:visible').click({ multiple: true });
     cy.wait(1000);
     cy.get('[data-auth="login"]').should('be.visible');
-    cy.get("button[data-auth='login']:visible").click();
+    cy.get("button[data-auth='login']:visible").click({ multiple: true });
     cy.wait(1500);
     cy.get("input[type='email']:visible")
-      .should("exist")
-      .type("thor@noroff.no");
-    cy.get("input[type='password']:visible").should("exist").type("Abc123456");
-    cy.get(".btn-success:visible").click();
+      .should('exist')
+      .type('thor@noroff.no');
+    cy.get("input[type='password']:visible").should('exist').type('Abc123456');
+    cy.get('.btn-success:visible').click({ multiple: true });
     cy.wait(3000);
     cy.then(
-      () => expect(window.localStorage.getItem("profile")).to.not.be.null
+      () => expect(window.localStorage.getItem('profile')).to.not.be.null,
     );
-    cy.then(() => expect(window.localStorage.getItem("token")).to.not.be.null);
-    cy.get("button[data-auth='logout']").click();
+    cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
+    cy.get("button[data-auth='logout']").click({ multiple: true });
     cy.get('[data-auth="login"]').should('be.visible');
-    cy.then(() => expect(window.localStorage.getItem("profile")).to.be.null);
-    cy.then(() => expect(window.localStorage.getItem("token")).to.be.null);
+    cy.then(() => expect(window.localStorage.getItem('profile')).to.be.null);
+    cy.then(() => expect(window.localStorage.getItem('token')).to.be.null);
   });
 });
